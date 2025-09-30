5410a0ad69b4a93df24e125cfe99c452cabd4ab3
Milan Louter <m.louter@student.tudelft.nl>
2025-09-30 15:27:06 +0200

    Modified run.py to plot signals.py before and after some operations applied

--- run.py
+++ run.py
@@ -1,7 +1,27 @@
 import matplotlib.pyplot as plt
 
-from signals import generate_sine_wave
-sine_wave = generate_sine_wave(frequency=5, duration=2, sample_rate=100)
+#import signals and operations
+from signals import generate_sine_wave, generate_step_function
+from operations import time_shift, time_scale
 
-plt.plot(sine_wave)
+#plot functions + after operations
+fs = 100 
+dur = 1.0
+sine = generate_sine_wave(5, dur, fs)
+step = generate_step_function(0.5, dur, fs)
+
+sine_shifted = time_shift(sine, fs, 0.3)
+
+step_scaled  = time_scale(step, 0.2)
+
+plt.figure()
+plt.plot(t, sine, label='sine')
+plt.plot(t, sine_shifted, '--', label='sine shifted')
+plt.legend(); plt.grid(); plt.title('Sine wave shifted')
+
+plt.figure()
+plt.plot(tp, step, label='step')
+plt.plot(tps, step_scaled, ':', label='step scaled')
+plt.legend(); plt.grid(); plt.title('Step function scaled')
+
 
