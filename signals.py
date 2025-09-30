78de4c5f21f71b553fe362c357406df79ddf4119
Milan Louter <m.louter@student.tudelft.nl>
2025-09-30 14:40:46 +0200

    adjusted small mistake in step function

--- signals.py
+++ signals.py
@@ -4,14 +4,15 @@ import numpy as np
 def generate_sine_wave(frequency, duration, sample_rate):
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     signal = np.sin(2 * np.pi * frequency * t)
-    return signal
+    return sine_wave
 
 
 # add step function
-def generate_step_signal(step_time, duration, sample_rate):
+def generate_step_function(step_time, duration, sample_rate):
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     signal = np.zeros_like(t)
     signal[t >= step_time] = 1.0
-    return signal
+    return step
+
 
 
